a55cd13a35cf0101ce722b53c5ac80e5579b992e
heyyakash <akashsharma2002@gmail.com>
2022-06-12 21:56:03 +0530

    fixed middleware address

--- backend/Routes/TaskRoute.js
+++ backend/Routes/TaskRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const task = require('../Schema/TasksSchema.js');
-const fetchuser = require('../middleware/fetchuser.js');
+const fetchuser = require('../Middleware/fetchuser.js');
 const {createTask,getTask,deleteTask, updateTask} = require('../Controllers/tasksController.js') 
 
 router.route('/').post(fetchuser,createTask).get(fetchuser,getTask);
