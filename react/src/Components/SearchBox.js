0ae6c8402f59e941a873d505664fe8c60586e795
heyyakash <akashsharma2002@gmail.com>
2022-06-12 16:00:02 +0530

    removed search bar

--- react/src/Components/SearchBox.js
+++ react/src/Components/SearchBox.js
@@ -12,10 +12,11 @@ const SearchBox = () => {
   }
   return (
     <div className='w-full flex'>
-        <div className='w-[300px] hidden lg:inline-flex h-[40px] bg-secondary items-center justify-around rounded-[200px]'>
+        {/* <div className='w-[300px] hidden lg:inline-flex h-[40px] bg-secondary items-center justify-around rounded-[200px]'>
             <input type="text" className='bg-transparent h-[30px] outline-none' placeholder='Search' />
             <BsSearch className='text-gray-400' />
-        </div>
+        </div> */}
+        <h1 className='text-2xl font-semibold text-primary'>Your tasks</h1>
         <div onClick = {handleAdd} className='bg-[#ffbf49] h-[40px] flex-grow lg:flex-grow-0 cursor-pointer ml-auto hover:bg-white hover:text-[#ffbf49] font-bold hover:border-2 hover:border-[#ffbf49] transition-all duration-200 text-white rounded-[200px] flex justify-center items-center w-[180px]'>Add Task</div>
         {/* <div onClick={handleSignOut} className='bg-[#ffbf49] cursor-pointer hover:bg-white hover:text-[#ffbf49] font-bold hover:border-2 hover:border-[#ffbf49] transition-all duration-200 text-white rounded-[200px] flex justify-center items-center px-4 ml-2'>Sign Out</div> */}
     </div>
