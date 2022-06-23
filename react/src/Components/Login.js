a5c1fa8b4e3fec32886fbcbbd362a354801a5bfc
heyyakash <akashsharma2002@gmail.com>
2022-06-23 14:58:01 +0530

    Fixed login button added some text

--- react/src/Components/Login.js
+++ react/src/Components/Login.js
@@ -1,80 +1,87 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Alert from './Alert';
 
 
 const Login = () => {
-    const [disabled,setDisabled] =useState(false);
+    const [disabled, setDisabled] = useState(false);
     const [success, setSuccess] = useState(false);
     const [show, setShow] = useState(false);
     const [msg, setMsg] = useState("");
     const navigate = useNavigate();
     useEffect(() => {
-      const token = localStorage.getItem('token');
-      if(token){
-          navigate('/');
-      }
+        const token = localStorage.getItem('token');
+        if (token) {
+            navigate('/');
+        }
     })
-    
-    const handleSubmit = async(e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
+        try {
             const url = `${process.env.REACT_APP_URL}/api/v1/login`;
             setDisabled(true);
             const res = await fetch(url,
                 {
-                    method:'POST',
-                    headers:{
-                        'Content-Type':'application/json'
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
                     },
-                    body:JSON.stringify(
+                    body: JSON.stringify(
                         {
-                            username:uname,
-                            password:pass
+                            username: uname,
+                            password: pass
                         }
                     )
                 })
             const data = await res.json();
-            if(data.success){
-                localStorage.setItem('token',data.authToken);
+            if (data.success) {
+                localStorage.setItem('token', data.authToken);
                 setDisabled(false);
                 navigate('/');
             }
-            else{
+            else {
                 setSuccess(false);
                 setMsg(data.msg);
                 setShow(true);
+                setDisabled(false);
                 setTimeout(() => {
                     setShow(false);
                     setSuccess(false);
                     setMsg("");
                 }, 2000);
-                
+
             }
         }
-        catch(err){
+        catch (err) {
             console.log(err)
         }
     }
-    const [uname,setUname] = useState("");
-    const [pass,setPass] = useState("");
+    const [uname, setUname] = useState("");
+    const [pass, setPass] = useState("");
 
     return (
         <div className='w-[100%] h-[100vh] fixed bg-primary flex justify-center items-center'>
-            <Alert msg={msg} success = {success} show={show} />
-            <div className='bg-white drop-shadow-2xl rounded-md p-[2rem] justify-center w-[300px]'>
-            <form className='flex flex-col justify-center items-start gap-3 '>
-                Username: <input type="text" value = {uname} onChange = {(e)=>setUname(e.target.value)} className = "bg-secondary w-full h-[30px] outline-primary px-2" />
-                Password: <input type="password" value = {pass} onChange = {(e)=>setPass(e.target.value)} className = "bg-secondary w-full h-[30px] outline-primary px-2" />
-                <div className="flex gap-2 mt-2">
-                <button onClick={handleSubmit} disabled = {uname.length===0 || pass.length===0 || disabled} className = "disabled:cursor-not-allowed disabled:opacity-60 bg-primary transition-all duration-150 text-white py-1 px-2 hover:text-primary hover:bg-white cursor-pointer rounded-sm" type = "submit">Sign In</button>
-                <button onClick={()=>navigate('/create')} className = "bg-secondary transition-all duration-150 text-primary py-1 px-2 hover:text-secondary hover:bg-primary cursor-pointer rounded-sm">Sign Up</button>
+            <Alert msg={msg} success={success} show={show} />
+            <div className='p-8 bg-white flex gap-2 rounded-xl'>
+                <div className="p-2 max-w-[500px]">
+                    <h2 className="text-2xl text-primary">Task Manager App</h2>
+                    <p className="my-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium optio non saepe adipisci hic nulla quia dicta expedita quas facilis dolore reprehenderit molestiae, earum, dolores ea ipsam voluptas. Non veniam corrupti molestias repellendus ea. Vero corrupti tenetur iusto deserunt dicta facere? Explicabo voluptatem minima quod debitis. Delectus ratione nesciunt dolore!</p>
                 </div>
-                
-            </form>
+                <div className='bg-white drop-shadow-xl rounded-md p-[2rem] justify-center w-[300px]'>
+                    <form className='flex flex-col justify-center items-start gap-3 '>
+                        <h2 className = 'text-xl'>Sign In</h2>
+                        Username: <input type="text" value={uname} onChange={(e) => setUname(e.target.value)} className="bg-secondary w-full h-[30px] outline-primary px-2" />
+                        Password: <input type="password" value={pass} onChange={(e) => setPass(e.target.value)} className="bg-secondary w-full h-[30px] outline-primary px-2" />
+                        <div className="flex gap-2 mt-2">
+                            <button onClick={handleSubmit} disabled={uname.length === 0 || pass.length === 0 || disabled} className="disabled:cursor-not-allowed disabled:opacity-60 bg-primary transition-all duration-150 text-white py-1 px-2 hover:text-primary hover:bg-white cursor-pointer rounded-sm" type="submit">Sign In</button>
+                            <button onClick={() => navigate('/create')} className="bg-secondary transition-all duration-150 text-primary py-1 px-2 hover:text-secondary hover:bg-primary cursor-pointer rounded-sm">Sign Up</button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
-            
         </div>
     )
 }
